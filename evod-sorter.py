fbede9d598b3bf738ba5a3f100433594d8ecce24
rei-kaizen <111290781+rei-kaizen@users.noreply.github.com>
2023-04-23 19:48:05 +0800

    update numbers

--- evod-sorter.py
+++ evod-sorter.py
@@ -14,10 +14,11 @@ class EvodSort:
         even_file.close()
         odd_file.close()
 
-EvodSort()
-
 #loop through the line in the file
 #loop through the numbers
 #assess if even or odd
 ##if even, add the numbers to the even list then write the numbers to the  even file
 ##else, add the numbers to the odd list  write the numbers to the odd file
+
+EvodSort()
+
